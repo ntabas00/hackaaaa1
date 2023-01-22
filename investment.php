8121d387503b39920170903499adbc4d707d1c97
nabila tabassum <68863566+ntabas00@users.noreply.github.com>
2023-01-22 10:24:27 -0500

    Add files via upload

---
+++ investment.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+
+</head>
+<body>
+
+<style>
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: 8%;
+        background-color: orange;
+        color: white;
+        text-align: center;
+    }
+    .header {
+
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: 0%;
+        background-color: orange;
+        color: white;
+        text-align: center;
+    }
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+    }
+
+    li a {
+        display: block;
+        width: 250px;
+        height:250px;
+        background-color: #dddddd;
+    }
+    px-2 {
+        padding-left: 500px;
+    }
+    previous {
+        background-color: #f1f1f1;
+        color: black;
+    }
+
+</style>
+
+</head>
+
+
+</body>
+<div class="header">
+    <h2 class="text-center">Investment</h2>
+</div>
+
+
+  <section class="my-5">
+           <div class="py-5">
+                 <h2 class="text-center">Financing Efficiently !!! </h2>
+           </div>
+      <div class="container-fluid">
+          <div class="row">
+              <div class=" col-lg-4  col-md-4 col-12">
+                  <div class="card">
+                      <img class= "card-img-top" src="p7.jpg" alt="Card image" >
+                      <div class="card-body">
+                          <h4 class="card-title">RRSP</h4>
+                          <p class="card-text">Allocate money into your saving accounts.</p>
+                          <a href="https://www.canada.ca/en/revenue-agency/services/tax/individuals/topics/rrsps-related-plans/registered-retirement-savings-plan-rrsp.html">Click to know more ....</a>
+<!--                          <a href="Aboutus.php" class="btn btn-primary">Click to know more</a>-->
+                      </div>
+                  </div>
+              </div>
+
+
+              <div class=" col-lg-4  col-md-4 col-12">
+                  <div class="card">
+                      <img class= "card-img-top" src="p9.jpg" alt="Card image">
+                      <div class="card-body">
+                          <h4 class="card-title">Future Saving</h4>
+                          <p class="card-text">Emergency funds.</p>
+                          <a href="https://itools-ioutils.fcac-acfc.gc.ca/BP-PB/budget-planner">Click to know more ....</a>
+
+
+                          <div class="py-3"></div>
+                          <a href="QUIZ.PHP" class="btn btn-success">Previous</a>
+                          <a href="Aboutus.php" class="btn btn-primary">Click Next</a>
+
+
+
+                      </div>
+                  </div>
+              </div>
+
+              <div class=" col-lg-4  col-md-4 col-12">
+                  <div class="card">
+                      <img class= "card-img-top" src="p10.jpg" alt="Card image">
+                      <div class="card-body">
+                          <h4 class="card-title">Invest Into stocks</h4>
+                          <p class="card-text">Planning for extra income</p>
+                          <a href="https://www.questrade.com/self-directed-investing/investment-products">Click to know more ....</a>
+<!--                          <a href="Aboutus.php" class="btn btn-primary">Click to know more</a>-->
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+      </div>
+  </section>
+
+
+
+
+
+<div class="footer">
+    <h2>@ConU hacks</h2>
+</div>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
